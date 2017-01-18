189ebfb0a12a02a28d9ad25719e79bb3279c77f0
Gerrod Mozeik <gerrodmozeik@zipcoders-MacBook-Pro-9.local>
2017-01-18 17:21:10 -0500

    Array rotation works

--- Lab1/src/main/java/ArrayRotator.java
+++ Lab1/src/main/java/ArrayRotator.java
@@ -1,13 +1,39 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
  * Created by gerrodmozeik on 1/18/17.
  */
-public class ArrayRotator {
+public class ArrayRotator extends ArrayList {
+
     public static void main(String[] args) {
-        List list = Arrays.asList("one Two three Four five six".split(" "));
-        System.out.println("List :"+list);
-        Collections.rotate(list, 1);
-        System.out.println("rotate: " + list);
+        ArrayRotator arrayRotator = new ArrayRotator();
+        int arr[] = {1, 2, 3, 4};
+        arrayRotator.rotateLeft(arr, 2, 4);
+        arrayRotator.printArray(arr, 4);
     }
+
+        public void rotateLeft(int arr[], int rotateBy, int arrayLength) {
+            for (int i = 0; i < rotateBy; i++) {
+                rotateLeftByOne(arr, arrayLength);
+            }
+        }
+
+        public void rotateLeftByOne(int arr[], int arrayLength) {
+            int j, swap;
+            swap = arr[0];
+            for (j = 0; j < arrayLength - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[j] = swap;
+        }
+
+
+        public void printArray(int arr[], int arrayLength) {
+            int h;
+            for (h = 0; h < arrayLength; h++) {
+                System.out.print(arr[h] + " ");
+            }
+        }
 }
+
