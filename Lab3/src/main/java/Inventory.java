e0758b65da76024a40a976edc717166a41441add
Gerrod Mozeik <gerrodmozeik@zipcoders-MacBook-Pro-9.local>
2017-01-18 18:30:07 -0500

    Started Lab 3

---
+++ Lab3/src/main/java/Inventory.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Created by gerrodmozeik on 1/18/17.
+ */
+public class Inventory {
+
+    ArrayList products = new ArrayList(1);
+    double totalValueOfAllProducts;
+
+    public double getTotalValueOfAllProducts() {
+        for (int i = 1; i <= products.size(); i++) {
+            
+        }
+    }
+}
---
+++ Lab3/src/main/java/Product.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gerrodmozeik on 1/18/17.
+ */
+public class Product {
+
+    double price;
+    String productID;
+    double amountOnHand;
+
+}
